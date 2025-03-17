48e43d2cc4165ba9fbce6c5609d54685c3f439a7
Adisorn_Sommart_12 <157354861+Variable62@users.noreply.github.com>
2025-03-17 23:56:10 +0700

    Update Assignment_1_Interrupt.ino

--- PART2/src/Assignment_1_Interrupt.ino
+++ PART2/src/Assignment_1_Interrupt.ino
@@ -1,9 +1,7 @@
-#define INT0_vect _VECTOR(1)        /* External Interrupt Request 0 */
+#define INT0_vect _VECTOR(1) //Switch
 #define TIMER1_OVF_vect _VECTOR(13) /* Timer 1 Overflow */
-#define F_CPU 16000000UL
 #include  <avr/io.h>
 #include  <util/delay.h>
-#define Sw PD2
 #define Led_Green PB0
 #define Led_Yellow PB1
 #define Led_Red PB2
@@ -30,7 +28,6 @@ else if(PORTB & (1 << Led_Yellow)){
 }
 int main() 
 { 
-  DDRD &= ~(1 << Sw);// Sw input
   DDRB  |= ((1 << Led_Red) | (1 << Led_Yellow) | (1 << Led_Green));// led out put
 	 
   EICRA = EICRA | 0b00000010; // INT0 Falling Edge
